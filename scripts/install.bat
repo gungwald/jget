3b99827929edd47b213370e3c1945d32bc29341c
Bill Chatfield <bill.chatfield@cardinalhealth.com>
2019-08-01 13:11:40 -0400

    Fixed install.bat

--- scripts/install.bat
+++ scripts/install.bat
@@ -3,14 +3,15 @@
 setlocal
 
 set MY_DIR=%~dp0
-set APP_NAME=find-non-ascii
-set LIB_DIR=%MY_DIR%..\lib
+set APP_NAME=jget
+set LIB_DIR=%MY_DIR%..\build\libs
+set APP_VER=1.0
 
 if not exist "%USERPROFILE%\bin" mkdir "%USERPROFILE%\bin"
 if not exist "%USERPROFILE%\lib" mkdir "%USERPROFILE%\lib"
 
 copy "%MY_DIR%%APP_NAME%.bat" "%USERPROFILE%\bin"
-copy "%LIB_DIR%\%APP_NAME%.jar" "%USERPROFILE%\lib"
+copy "%LIB_DIR%\%APP_NAME%-%APP_VER%.jar" "%USERPROFILE%\lib"
 
 "%MY_DIR%add-to-path" "%USERPROFILE%\bin"
 
